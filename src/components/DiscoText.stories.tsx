4e15bf537dfa0f4dacf63a554c1d14929a045d37
EC2 Default User <ec2-user@ip-172-31-39-14.us-east-2.compute.internal>
2022-05-18 19:27:02 +0000

    initial commit

--- src/components/DiscoText.stories.tsx
+++ src/components/DiscoText.stories.tsx
@@ -10,7 +10,7 @@ export default {
 
 const Template: ComponentStory<typeof DiscoText> = () => (
   <Typography variant="h1">
-    <DiscoText>Disco</DiscoText>
+    <DiscoText>Test</DiscoText>
   </Typography>
 );
 
